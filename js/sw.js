538b1db56d92f3668ed3f7c5f88a285a2b56784b
Seamus Connolly <sghconnolly@gmail.com>
2018-05-18 13:07:10 +0100

    feat: Add install service worker code

--- js/sw.js
+++ js/sw.js
@@ -8,7 +8,24 @@ self.addEventListener('install', function(event) {
         // The caches API allows us to store static files for retrieval later
         caches.open('cacheName').then(function(cache) {
             return cache.addAll([
-                '../index.html'
+                '../index.html',
+                '../restaurant.html',
+                'dbhelper.js',
+                'restaurant_info.js',
+                'main.js',
+                '../css/styles.css',
+                '../css/normalize.css',
+                '../img/1.jpg',
+                '../img/2.jpg',
+                '../img/3.jpg',
+                '../img/4.jpg',
+                '../img/5.jpg',
+                '../img/6.jpg',
+                '../img/7.jpg',
+                '../img/8.jpg',
+                '../img/9.jpg',
+                '../img/10.jpg',
+                '../data/restaurants.json'
             ]);
         })
     );
