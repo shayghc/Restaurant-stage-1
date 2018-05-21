0f7e5bac3d479e3b6a79bb048678991e2322e6d6
Seamus Connolly <sghconnolly@gmail.com>
2018-05-21 08:44:04 +0100

    chore: Move sw.js file location and update asset paths in sw.js

--- sw.js
+++ sw.js
@@ -8,24 +8,25 @@ self.addEventListener('install', function(event) {
         // The caches API allows us to store static files for retrieval later
         caches.open('cacheName').then(function(cache) {
             return cache.addAll([
-                '../index.html',
-                '../restaurant.html',
-                'dbhelper.js',
-                'restaurant_info.js',
-                'main.js',
-                '../css/styles.css',
-                '../css/normalize.css',
-                '../img/1.jpg',
-                '../img/2.jpg',
-                '../img/3.jpg',
-                '../img/4.jpg',
-                '../img/5.jpg',
-                '../img/6.jpg',
-                '../img/7.jpg',
-                '../img/8.jpg',
-                '../img/9.jpg',
-                '../img/10.jpg',
-                '../data/restaurants.json'
+                '/',
+                'index.html',
+                'restaurant.html',
+                'js/dbhelper.js',
+                'js/restaurant_info.js',
+                'js/main.js',
+                'css/styles.css',
+                'css/normalize.css',
+                'img/1.jpg',
+                'img/2.jpg',
+                'img/3.jpg',
+                'img/4.jpg',
+                'img/5.jpg',
+                'img/6.jpg',
+                'img/7.jpg',
+                'img/8.jpg',
+                'img/9.jpg',
+                'img/10.jpg',
+                'data/restaurants.json'
             ]);
         })
     );
